840ff1db42a86dfd6d066900be79389b5d3037f8
7LGnGZkY <n35.nnlpd@slmail.me>
2022-06-17 20:12:43 +0100

    Exercise complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const AssertionList = require("./src/AssertionList.js")
-const Equal = require("./src/Equal.js")
-const Include = require("./src/Include.js")
+// Already completed
 
-const equalAssertion = new Equal(1, 1)
-const includeAssertion = new Include([1,2,3], 3)
-const assertionList = new AssertionList([equalAssertion, includeAssertion])
+const AssertionList = require('./src/AssertionList.js');
+const Equal = require('./src/Equal.js');
+const Include = require('./src/Include.js');
 
-console.log(assertionList.check())
+const equalAssertion = new Equal(1, 1);
+const includeAssertion = new Include([1, 2, 3], 3);
+const assertionList = new AssertionList([equalAssertion, includeAssertion]);
+
+console.log(assertionList.check());
